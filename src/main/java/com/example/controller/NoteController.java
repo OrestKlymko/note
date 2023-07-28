f1722276e76abbcce6767d254b6f69cdac07cf4d
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-07-28 14:43:11 +0200

    first commit

--- src/main/java/com/example/controller/NoteController.java
+++ src/main/java/com/example/controller/NoteController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 
 import com.example.crudCommand.NoteService;
 import com.example.entity.Note;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/note")
 public class TestController {
-	private NoteService noteService = new NoteService(); // видалю як буде підключено бд, поки для дебагу
+	private final NoteService noteService;
 
 	@GetMapping("/list")
 	public ModelAndView getAllNotes() {
@@ -27,6 +30,15 @@ public class TestController {
 		ModelAndView modelAndView = new ModelAndView("edit-note");
 		modelAndView.addObject("noteEdit", noteService.getById(Long.parseLong(id)));
 		return modelAndView;
+	}
+
+	@PostMapping("/create")
+	public ModelAndView createNote(@RequestParam("title") String title, @RequestParam("content") String content) {
+		Note note = new Note();
+		note.setTitle(title);
+		note.setContent(content);
+		noteService.add(note);
+		return new ModelAndView("redirect:/note/list");
 	}
 
 	@PostMapping("/edit")
