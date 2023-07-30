70ca9bd28d08766578ad69cc291fc8b1b67d28bb
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-07-30 14:09:16 +0200

    upd security

--- src/main/java/com/example/controller/NoteController.java
+++ src/main/java/com/example/controller/NoteController.java
@@ -5,10 +5,7 @@ import com.example.crudCommand.NoteService;
 import com.example.entity.Note;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
